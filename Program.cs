867032cc1ca3e1af696e3bc6e5c74687ac501f4e
Aquiles Aguiar <aquilesvibe@gmail.com>
2021-10-11 19:56:54 -0300

    primeiro commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DapaDale_TinderUCl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            apiCep cep = new apiCep("29164370");
+            cep.retornaLocalidade();
+            Console.WriteLine(cep.retornaLocalidade().cep);
+        }
+    }
+}
---
+++ apiCep.cs
@@ -0,0 +1,33 @@
+using System;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json; 
+using Newtonsoft.Json.Serialization;
+
+namespace DapaDale_TinderUCl
+{
+    public class apiCep
+    {   
+        string Cep;
+        public apiCep(string cep){
+            Cep = cep;
+        }
+
+        public ResponseViaCep retornaLocalidade(){
+            
+            // Joga pra string novamente
+            // var json2 = JsonConvert.SerializeObject(viacep, Formatting.Indented, settings);     
+            var cliente = new WebClient();
+            var text = cliente.DownloadString("https://viacep.com.br/ws/"+Cep+"/json/");
+
+            var settings = new JsonSerializerSettings
+            {
+                PreserveReferencesHandling = PreserveReferencesHandling.Objects,
+            };
+            ResponseViaCep viacep = JsonConvert.DeserializeObject<ResponseViaCep>(text);
+
+            return viacep;
+        }
+    }
+        
+}

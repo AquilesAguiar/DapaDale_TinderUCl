4fb544dcdd880bd3b6b427917ed5eba1ff3755a1
Gabriel De Souza Passos <bielpassos@hotmail.com>
2021-10-14 20:09:44 -0300

    Recomendações
    Att

--- controleMenu.cs
+++ controleMenu.cs
@@ -70,6 +70,7 @@ ________________________________________________________________________________
                     Console.WriteLine(@$"___________________________________________________________________________________________________________________________________________
                                 Foto:{user.Foto}
                                 Nome:{user.Nome}
+                                Descriçao:{user.Descricao}
                                 Data de Nascimento:{user.DataNasc}
                                 Rua:{user.Endereco.logradouro}
                                 Bairro:{user.Endereco.bairro}
@@ -96,9 +97,66 @@ ________________________________________________________________________________
             }
         }
 
+        public static void recomendacao(string Usu, string Senha,List<Usuario> Usuarios){
+            string rua,bairro,estado;
+            string[] interesses = new string[4];
+            foreach (Usuario user in Usuarios)
+            {
+                if (user.Nome == Usu && user.Senha == Senha)
+                { 
+                    rua = user.Endereco.logradouro;
+                    bairro = user.Endereco.bairro;
+                    estado = user.Endereco.uf;
+
+                    interesses[0] = user.interessesUser[0] ;
+                    interesses[1] = user.interessesUser[1] ;
+                    interesses[2] = user.interessesUser[2] ;
+                    interesses[3] = user.interessesUser[3] ;
+                }   
+            }
+                
+            foreach (Usuario user in Usuarios)
+            {
+                int cont = 0;   
+                if (user.Nome != Usu && user.Senha != Senha)
+                {
+                    if(user.Endereco.logradouro == rua){
+                        cont++;
+                    }
+                    if(user.Endereco.bairro == bairro){
+                        cont++;
+                    }
+                    if(user.Endereco.uf == estado){
+                        cont++;
+                    }
+
+                    if(interesses[0] == user.interessesUser[0]){
+                        cont++;
+                    }
+                    if(interesses[1] == user.interessesUser[1]){
+                        cont++;
+                    }
+                    
+                    if(interesses[2] == user.interessesUser[2]){
+                        cont++;
+                    }
+                    
+                    if(interesses[3] == user.interessesUser[3]){
+                        cont++;
+                    }
+
+                }
+            }
+        }
         public static void darLikes(string Usu, string Senha,List<Usuario> Usuarios){
             while(true){
-                break;
+                double x1, x2, y1, y2;
+                x1 = 12d;
+                x2 = 13d;
+                y1 = 11d;
+                y2 = 10d;
+                var distance = Math.Sqrt((Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2)));
+                Console.WriteLine(distance);
             }
         }
         

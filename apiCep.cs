d42aa132fe267db63e1e4e5ddb468a7119f57567
Aquiles Aguiar <80758907+AquilesCedisa@users.noreply.github.com>
2021-10-15 15:05:04 -0300

    recomendacoes ordernada

--- apiCep.cs
+++ apiCep.cs
@@ -16,13 +16,19 @@ namespace DapaDale_TinderUCl
         public ResponseViaCep retornaLocalidade(){
             
             // Joga pra string novamente
-            // var json2 = JsonConvert.SerializeObject(viacep, Formatting.Indented, settings);     
-            var cliente = new WebClient();
-            var text = cliente.DownloadString("https://viacep.com.br/ws/"+Cep+"/json/");
+            // var json2 = JsonConvert.SerializeObject(viacep, Formatting.Indented, settings);
+            try
+            {    
+                var cliente = new WebClient();
+                var text = cliente.DownloadString("https://viacep.com.br/ws/"+Cep+"/json/");
+                ResponseViaCep viacep = JsonSerializer.Deserialize<ResponseViaCep>(text);
 
-            ResponseViaCep viacep = JsonSerializer.Deserialize<ResponseViaCep>(text);
-
-            return viacep;
+                return viacep;
+            }
+            catch (System.Exception)
+            {
+                throw;
+            }     
         }
     }
         
--- controleMenus.cs
+++ controleMenus.cs
@@ -1,6 +1,6 @@
 using System;
-using System.IO;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace DapaDale_TinderUCl
 {   
@@ -76,6 +76,8 @@ ________________________________________________________________________________
                                 Bairro:{user.Endereco.bairro}
                                 Estado:{user.Endereco.uf}
 ___________________________________________________________________________________________________________________________________________");
+                    Console.ReadKey();
+                    Console.Clear();
                     break;
                 }
             }
@@ -92,17 +94,20 @@ ________________________________________________________________________________
                                 3 - {user.interessesUser[2]}
                                 4 - {user.interessesUser[3]}
 ___________________________________________________________________________________________________________________________________________");
+                    Console.ReadKey();
+                    Console.Clear();
                     break;
                 }
             }
         }
 
-        public static Dictionary<string, Tuple<int,int>> recomendacao(string Usu, string Senha,List<Usuario> Usuarios){
+        public static Dictionary<Tuple<string,string>, Tuple<int,int>> recomendacao(string Usu, string Senha,List<Usuario> Usuarios){
             string rua = "",bairro = "",estado = "";
             string[] interesses = new string[4];
-            Dictionary<string, Tuple<int,int>> recomendacoesUsers = new Dictionary<string, Tuple<int,int>>();
+            Dictionary<Tuple<string,string>, Tuple<int,int>> recomendacoesUsers = new Dictionary<Tuple<string,string>, Tuple<int,int>>();
             foreach (Usuario user in Usuarios)
             {
+                
                 if (user.Nome == Usu && user.Senha == Senha)
                 { 
                     rua = user.Endereco.logradouro;
@@ -116,14 +121,13 @@ ________________________________________________________________________________
                     break;
                 }   
             }
-                
+        
             foreach (Usuario user2 in Usuarios)
             {
                 int cont = 0;   
                 int cont2 = 0;
-                if (user2.Nome != Usu && user2.Senha != Senha)
+                if (user2.Nome != Usu || user2.Senha != Senha)
                 {   
-                    Console.WriteLine(user2.Nome);
                     if(user2.Endereco.logradouro == rua){
                         cont++;
                     }
@@ -134,72 +138,75 @@ ________________________________________________________________________________
                         cont++;
                     }
 
-                    if(interesses[0] == user2.interessesUser[0]){
-                        cont2++;
+                    for (int i = 0; i < 4; i++)
+                    {
+                        if(user2.interessesUser.Contains(interesses[i])){
+                            cont2++;
+                        };
                     }
-                    if(interesses[1] == user2.interessesUser[1]){
-                        cont2++;
-                    }
-                    
-                    if(interesses[2] == user2.interessesUser[2]){
-                        cont2++;
-                    }
-                    
-                    if(interesses[3] == user2.interessesUser[3]){
-                        cont2++;
-                    }
-
-                    recomendacoesUsers.Add(user2.Nome + user2.Senha, new Tuple<int, int>(cont,cont2));
+                    recomendacoesUsers.Add(new Tuple<string, string>(user2.Nome,user2.Senha), new Tuple<int, int>(cont,cont2));
                 }
             }
             return recomendacoesUsers;
         }
-        public static void darLikes(string Usu, string Senha,List<Usuario> Usuarios){
-            Dictionary<string, Tuple<int,int>> recomendacoes = recomendacao(Usu,Senha,Usuarios); 
-            foreach( KeyValuePair<string, Tuple<int,int>> kvp in recomendacoes )
+
+        public static Dictionary<Tuple<string,string>, double> calcRecomendacao(string Usu, string Senha,List<Usuario> Usuarios){
+            Dictionary<Tuple<string,string>,Tuple<int,int>> recomendacoes = recomendacao(Usu,Senha,Usuarios); 
+            Dictionary<Tuple<string,string>, double> distanciaCalc = new Dictionary<Tuple<string,string>, double>();
+            
+            foreach( KeyValuePair<Tuple<string,string>, Tuple<int,int>> kvp in recomendacoes )
             {
-                Console.WriteLine("Key = {0}, Value = {1}",kvp.Key, kvp.Value);
+
+                int x1, x2, y1, y2;
+                x1 = 3;
+                x2 = kvp.Value.Item1;
+                y1 = 4;
+                y2 = kvp.Value.Item2;
+                
+                var distance = Math.Sqrt((Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2)));
+                distanciaCalc.Add(new Tuple<string, string>(kvp.Key.Item1,kvp.Key.Item2),Math.Round(distance,2));
             }
-            // while(true){
-            //     Dictionary<string, Tuple<int,int>> recomendacoes = recomendacao(Usu,Senha,Usuarios); 
-            //     foreach( KeyValuePair<string, Tuple<int,int>> kvp in recomendacoes )
-            //     {
-            //         Console.WriteLine("Key = {0}, Value = {1}",kvp.Key, kvp.Value);
-            //     }
-                // double x1, x2, y1, y2;
-                // x1 = 12d;
-                // x2 = 13d;
-                // y1 = 11d;
-                // y2 = 10d;
-                // var distance = Math.Sqrt((Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2)));
-                // Console.WriteLine(distance);
-                foreach(Usuario user in Usuarios){
-                    if (user.Nome != Usu && user.Senha != Senha){
+            var sortedDict = from entry in distanciaCalc orderby entry.Value ascending select entry;
+            
+            distanciaCalc.Clear();
 
-                        Console.WriteLine(@$"___________________________________________________________________________________________________________________________________________
-                                Foto:{user.Foto}
-                                Nome:{user.Nome}
-                                Descriçao:{user.Descricao}
-                                Data de Nascimento:{user.DataNasc}
-                                Rua:{user.Endereco.logradouro}
-                                Bairro:{user.Endereco.bairro}
-                                Estado:{user.Endereco.uf}
-___________________________________________________________________________________________________________________________________________");
-                        Console.WriteLine("\n\n");
+            foreach( KeyValuePair<Tuple<string,string>, double> kvp in sortedDict )
+            {   
+                distanciaCalc.Add(new Tuple<string,string>(kvp.Key.Item1,kvp.Key.Item2), kvp.Value);
+            }
+            return distanciaCalc;
+        }
+        public static void darLikes(string Usu, string Senha,List<Usuario> Usuarios){
+            Dictionary<Tuple<string,string>, double> recomendacoes = calcRecomendacao(Usu,Senha,Usuarios);
+            foreach( KeyValuePair<Tuple<string,string>, double> kvp in recomendacoes){
+//                 if (user.Nome != Usu && user.Senha != Senha){
 
-                        Console.WriteLine("Like(Pressione L) || Dislike(Pressione D)");
-                        ConsoleKeyInfo situacao = Console.ReadKey();
-                        if(situacao.KeyChar == 'l'){
-                            Console.WriteLine("Gostou!");
-                        }else if( situacao.KeyChar == 'd'){
-                            Console.WriteLine("Não Gostou!");
-                        }
-                    }
-                }
-                    
-        
+//                     Console.WriteLine(@$"___________________________________________________________________________________________________________________________________________
+//                             Foto:{user.Foto}
+//                             Nome:{user.Nome}
+//                             Descriçao:{user.Descricao}
+//                             Data de Nascimento:{user.DataNasc}
+//                             Rua:{user.Endereco.logradouro}
+//                             Bairro:{user.Endereco.bairro}
+//                             Estado:{user.Endereco.uf}
+// ___________________________________________________________________________________________________________________________________________
+//                                     Interessses
+//                             1 - {user.interessesUser[0]}
+//                             2 - {user.interessesUser[1]}
+//                             3 - {user.interessesUser[2]}
+//                             4 - {user.interessesUser[3]}
+// ___________________________________________________________________________________________________________________________________________");
+//                     Console.WriteLine("\n\n");
 
-            // }
+//                     Console.WriteLine("Dislike(Pressione A) || Like(Pressione D)");
+//                     ConsoleKeyInfo situacao = Console.ReadKey();
+//                     if(situacao.KeyChar == 'd'){
+//                         Console.WriteLine("Gostou!");
+//                     }else if( situacao.KeyChar == 'a'){
+//                         Console.WriteLine("Não Gostou!");
+//                     }
+//                 }
+            }
         }
         
         
